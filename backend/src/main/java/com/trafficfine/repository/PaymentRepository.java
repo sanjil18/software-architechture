dc4bfb5c78fa61b71728484d823e4fd204415446
sanjil18 <eg245320@engug.ruh.ac.lk>
2026-06-10 08:51:38 +0530

    corrected

--- backend/src/main/java/com/trafficfine/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trafficfine.repository;
-
-import com.trafficfine.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
